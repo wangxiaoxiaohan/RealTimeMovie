9ef14bb1149b0ab94e85f72f8fa0572b3781029c
wanghan <942732983@qq.com>
2018-02-13 10:25:30 +0800

    第二次提交

---
+++ app/src/main/java/example/wanghan/com/realtimemovie/Activity/PostImageActivity.java
@@ -0,0 +1,15 @@
+package example.wanghan.com.realtimemovie.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import example.wanghan.com.realtimemovie.R;
+
+public class PostImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_image);
+    }
+}
